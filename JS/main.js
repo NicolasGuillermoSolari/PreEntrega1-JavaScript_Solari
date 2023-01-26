921709e748e92d665f1bd16ae048bd03b505bae2
Nicolas Guillermo Solari <nicolasguillermosolari@gmail.com>
2023-01-26 20:34:18 -0300

    cree entrada para mayor edad

---
+++ JS/main.js
@@ -0,0 +1,8 @@
+/*Condicional*/
+const mayorDeEdad = prompt("Ingrese su edad por favor");
+if (mayorDeEdad >= 18) {
+  alert("acceso aprobado");
+} else {
+  alert("Debe ser mayor de edad para ingresar");
+}
+/*ciclo*/

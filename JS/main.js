b5a8ec7f22c0d01520d886b3a57a69e775a0d599
Nicolas Guillermo Solari <nicolasguillermosolari@gmail.com>
2023-01-26 20:35:29 -0300

    cree ciclo para nombres de inquilinos

--- JS/main.js
+++ JS/main.js
@@ -6,3 +6,12 @@ if (mayorDeEdad >= 18) {
   alert("Debe ser mayor de edad para ingresar");
 }
 /*ciclo*/
+let nombresInquilinos = prompt(
+  "ingrese nombre de una de las personas que va a alquilar, o escriba SALIR"
+);
+while (nombresInquilinos != "SALIR") {
+  alert("El nombre del inquilino es " + nombresInquilinos);
+}
+if ((nombresInquilinos = " ")) {
+  alert(" no puede dejar vacio el estacio, escriba SALIR");
+}

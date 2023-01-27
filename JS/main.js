a5eb0ccae4b4a165a4c6e587fde8a0c2e280417d
Nicolas Guillermo Solari <nicolasguillermosolari@gmail.com>
2023-01-26 21:25:58 -0300

    solucionado el ciclo

--- JS/main.js
+++ JS/main.js
@@ -11,7 +11,15 @@ let nombresInquilinos = prompt(
 );
 while (nombresInquilinos != "SALIR") {
   alert("El nombre del inquilino es " + nombresInquilinos);
+  nombresInquilinos = prompt(
+    "ingrese nombre de una de las personas que va a alquilar, o escriba SALIR"
+  );
 }
 if ((nombresInquilinos = " ")) {
   alert(" no puede dejar vacio el estacio, escriba SALIR");
 }
+
+const porcentajeComision = 0.2;
+const valorInmuebleAlquiler = prompt("ingrese valor del alquiler");
+const valorConComision = valorInmuebleAlquiler * porcentajeComision;
+alert("pagara: " + valorConComision);

7ad93c4d29dc60fcb2628b88f7089cf451b58097
Nicolas Guillermo Solari <nicolasguillermosolari@gmail.com>
2023-02-09 00:51:52 -0300

    agrego imagenes de logo y propiedades, agrego JS objeto constructor

--- JS/main.js
+++ JS/main.js
@@ -23,3 +23,27 @@ const porcentajeComision = 0.2;
 const valorInmuebleAlquiler = prompt("ingrese valor del alquiler");
 const valorConComision = valorInmuebleAlquiler * porcentajeComision;
 alert("pagara: " + valorConComision);
+
+const calcularAlquiler = function (precioAlquiler) {
+  if (precioAlquiler < 4 || precioAlquiler > 3) {
+    return 600;
+  } else if (precioAlquiler < 3 || precioAlquiler > 2) {
+    return 500;
+  } else if (precioAlquiler < 2 || precioAlquiler > 1) {
+    return 400;
+  }
+  return 200;
+};
+
+/*Objeto constructor*/
+function propiedadEnAlquiler(direccion, ambientes, precio) {
+  this.direccion = direccion;
+  this.ambientes = ambientes;
+  this.precio = precio;
+}
+
+const propiedadEnAlquiler1 = new propiedadEnAlquiler(
+  "Av.Libertador 6299 depto 815",
+  2,
+  1500
+);
